191a8cbee6b884211380f7324a70ddd3234e6b35
Nattapat <nattapat@Nattapats-MacBook-Air.local>
2024-10-31 12:11:40 +0700

    schema type add

--- src/types/common.ts
+++ src/types/common.ts
@@ -10,8 +10,41 @@ interface KeyValueObject {
     [key: string]: string | KeyValueObject
 }
 
+type Room = {
+    _id: string
+    currentQuestion: number
+    pin: string
+    hostID: string
+    status: string[]
+    players: Player[]
+    questions: Question[]
+}
+
+type Player = {
+    name: string
+    score: number
+}
+
+type Question = {
+    _id: string
+    name: string
+    timer: number
+    choices: Choice[]
+}
+
+type Choice = {
+    name: string
+    isCorrect: boolean
+    countPlayers: number
+}
+
 export type {
     LayoutProps,
     KeyValueObject,
     PageProps,
+    Room,
+    Player,
+    Question,
+    Choice,
 }
+
