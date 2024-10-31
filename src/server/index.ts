21b025d35c685cef20fca0e268fec0bcd255c199
Nan Jensathianwong <nan@thinknet.co.th>
2024-10-31 16:00:05 +0700

    fix build failed

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,7 +8,6 @@ import Next from 'next'
 import configs from '@/configs'
 import { setupHealthCheck } from '@/server/libs/health_check'
 import router from '@/server/router'
-import observer from '@thinknet/observer'
 
 const dev = configs.NODE_ENV !== 'production'
 
@@ -20,22 +19,14 @@ const start = async () => {
         await nextServer.prepare()
 
         expressServer
-            .use(observer.newrelic.middleware.captureRequestInfo as any)
             .use(Express.json())
             .use(Express.urlencoded({ extended: false }))
             .use(cookieParser())
-            .use(observer.trace())
-            .use(observer.collectDefaultMetrics())
-            .use(observer.accesslog())
 
         expressServer
             .use(configs.URL_PREFIX, router)
-            .get('/metrics', observer.getMetrics())
             .all('*', (req, res) => nextServer.getRequestHandler()(req, res))
         
-        expressServer
-            .use(observer.traceException())
-
         const serverListener = expressServer.listen(configs.NODE_PORT, () => {
             console.info(`Running on ${configs.APP_URL}`)
         })
