c38f266e4dfbd8031e4609dda662138162267051
Nattapat <nattapat@Nattapats-MacBook-Air.local>
2024-10-31 12:18:40 +0700

    init room before call api

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,36 @@
-export const LOCALE = {
-  TH: 'th',
-  EN: 'en',
-}
+import { Room } from "@/types/common";
 
-export const SUPPORTED_LOCALES = [LOCALE.TH, LOCALE.EN]
+export const LOCALE = {
+  TH: "th",
+  EN: "en",
+};
+
+export const SUPPORTED_LOCALES = [LOCALE.TH, LOCALE.EN];
+
+export const ROOM_INIT: Room = {
+  _id: "8913751",
+  currentQuestion: 0,
+  pin: "1234",
+  hostID: "123123123123",
+  status: "wait", // ["wait","countdown", "start", "summarize", "end"]
+  players: [
+    {
+      name: "Player 1",
+      score: 0,
+    },
+  ],
+  questions: [
+    {
+      _id: "123123123123",
+      name: "Question 1",
+      timer: 10,
+      choices: [
+        {
+          name: "Choice 1",
+          isCorrect: false,
+          countPlayers: 0,
+        },
+      ],
+    },
+  ],
+};
