523e2fee09a768d3368c6f32112798601369409c
Nattapat <nattapat@Nattapats-MacBook-Air.local>
2024-10-31 12:19:36 +0700

    update schema type

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,50 +1,49 @@
 interface PageProps {
-    params: {
-        locale: string
-    }
+  params: {
+    locale: string;
+  };
 }
 
-interface LayoutProps extends React.PropsWithChildren, PageProps { }
+interface LayoutProps extends React.PropsWithChildren, PageProps {}
 
 interface KeyValueObject {
-    [key: string]: string | KeyValueObject
+  [key: string]: string | KeyValueObject;
 }
 
 type Room = {
-    _id: string
-    currentQuestion: number
-    pin: string
-    hostID: string
-    status: string[]
-    players: Player[]
-    questions: Question[]
-}
+  _id: string;
+  currentQuestion: number;
+  pin: string;
+  hostID: string;
+  status: string;
+  players: Player[];
+  questions: Question[];
+};
 
 type Player = {
-    name: string
-    score: number
-}
+  name: string;
+  score: number;
+};
 
 type Question = {
-    _id: string
-    name: string
-    timer: number
-    choices: Choice[]
-}
+  _id: string;
+  name: string;
+  timer: number;
+  choices: Choice[];
+};
 
 type Choice = {
-    name: string
-    isCorrect: boolean
-    countPlayers: number
-}
+  name: string;
+  isCorrect: boolean;
+  countPlayers: number;
+};
 
 export type {
-    LayoutProps,
-    KeyValueObject,
-    PageProps,
-    Room,
-    Player,
-    Question,
-    Choice,
-}
-
+  LayoutProps,
+  KeyValueObject,
+  PageProps,
+  Room,
+  Player,
+  Question,
+  Choice,
+};
